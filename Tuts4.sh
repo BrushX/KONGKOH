e2a59d7a9d02fb9245cae77d26ac0d34bc6a681d
BrushX <40270106+BrushX@users.noreply.github.com>
2018-06-14 21:04:54 +0000

    Add files via upload

---
+++ Tuts4.sh
@@ -0,0 +1,28 @@
+toilet -f standard -F gay WELCOME!
+date|lolcat
+
+echo "\33[34;1mAuthor : MrTuts4"
+echo "\33[32;1mTeam : BrushX" 
+echo "\33[31;1mVersion : 0.1"
+
+sleep "1"
+
+echo "\33[33;1m+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo "1.Install LITESPAM"
+echo "\33[33;1m+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo "2.Install LITEDDOS"
+echo "\33[33;1m+-+-+-+-+-+-+-+-+-+-+-+-+"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+pkg install php
+pkg install toilet
+pkg install git
+pkg install python2
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+clear
+sh LITESPAM.sh
+fi
