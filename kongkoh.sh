1be9b0ded230ffa758d5b7a261abffc7a7a494c6
BrushX <40270106+BrushX@users.noreply.github.com>
2018-06-17 08:28:47 +0000

    Add files via upload

---
+++ kongkoh.sh
@@ -0,0 +1,185 @@
+toilet -f standard -F gay WELCOME!
+date|lolcat
+
+echo "\33[34;1mAuthor : MrTuts4"
+echo "\33[32;1mTeam : BrushX" 
+echo "\33[31;1mVersion : 0.1"
+
+sleep "3"
+
+echo "\33[36;1mTools BrusX"
+echo "1.Install santet-online"
+echo "2.Install GcoSpam"
+echo "3.Install LITESPAM"
+echo "4.Install CrewBot"
+echo "5.Install LITEDDOS"
+echo "6.Install IPGeolocation"
+echo "7.Install HackInsta"
+echo "8.Install Weeman"
+echo "9.Install Hunner"
+echo "10.Install New-Spammer"
+echo "11.Install BlackSpam"
+echo "12.Install SpazSMS"
+echo "13.Install Google-Dork"
+echo "\033[33;1m BrushX>:"
+read mrrm
+
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/Gameye98/santet-online
+ls
+cd santet-online
+clear
+python2 santet.py
+fi
+
+if [ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+apt upgrade && apt update
+apt install git
+git clone https://github.com/Amriez/gcospam
+cd gcospam
+sh install.sh
+sh gco.sh
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+pkg install php
+pkg install toilet
+pkg install sh
+pkg install git
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+apt update
+apt upgrade
+pkg install python2
+git clone https://github.com/Xeit666h05t/CrewBot
+ls
+cd CrewBot
+python2 CrewBot.py
+fi
+
+if [ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+apt update
+apt upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 liteDDOS.py
+fi
+
+if [ $mrrm = 6 ] || [ $mrrm = 6]
+then
+clear
+git clone https://github.com/maldevel/IPGeolocation
+cd IPGeolocation
+chmod +x ipgeolocation.py
+pip install -r requirements.txt
+python ipgeolocation.py -m
+python ipgeolocation.py -t IP 
+fi
+
+if [ $mrrm = 7 ] || [ $mrrm = 7 ]
+then
+clear
+pkg install python
+pkg install git
+git clone https://github.com/avramit/instahack
+ls
+cd instahack
+chmod +x hackinsta.py
+pip install -r pass.txt
+pip install -r password.txt
+pip install -r pasa.txt
+python hackinsta.py
+fi
+
+if [ $mrrm = 8 ] || [ $mrrm = 8 ]
+then
+clear
+apt update && apt upgrade -y
+apt install git -y
+apt install python2 -y
+git clone https://github.com/evait-security/weeman
+cd weeman
+chmod 777 weeman.py
+python2 weeman.py
+set url http://facebook.com
+set action_url http://facebook.com
+run
+fi
+
+if [ $mrrm = 9 ] || [ $mrrm = 9 ]
+then
+clear
+apt update
+apt install python
+apt install git -y
+git clone https://github.com/b3-v3r/Hunner
+cd Hunner
+chmod 777 hunner.py
+python hunner.py
+fi
+
+if [ $mrrm = 10 ] || [ $mrrm = 10 ]
+then
+clear
+pkg install git
+git clone https://github.com/haijuga7/New-Spammer
+cd New-Spammer
+unzip Spamm.zip
+sh spam.sh
+fi
+
+if [ $mrrm = 11 ] || [ $mrrm = 11 ]
+then
+clear
+pkg update && pkg upgrade 
+pkg install python2
+pkg install php
+pkg install toilet
+pkg install nano ruby figlet
+gem install lolcat
+pkg install git
+git clone  https://github.com/BLACheat78/spammblackheat78
+cd spammblackheat78
+sh Blackspamm.sh
+fi
+
+if [ $mrrm = 12 ] || [ $mrrm = 12 ]
+then
+clear
+pkg install git
+pkg install python2
+git clone https://github.com/Gameye98/SpazSMS
+cd SpazSMS
+ls
+python2 lite.py
+python2.7 lite.py 
+fi
+
+if [ $mrrm = 13 ] || [ $mrrm = 13 ]
+then
+pkg install git
+pkg install python2
+git clone https://github.com/XG77Z10/Google-Dork
+pip2 install termcolor
+cd Google-Dork
+python2 Google.py
+fi
